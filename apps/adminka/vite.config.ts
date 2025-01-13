d1945c7ae8f7a2198a291acef4da736f757c31eb
hrustalq <n1k3f1t@gmail.com>
2025-01-13 10:28:50 +0300

    setup for tanstack router

--- apps/adminka/vite.config.ts
+++ apps/adminka/vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+import path from 'path'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   server: {
     port: 8080,
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
   },
 })
